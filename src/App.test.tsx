bd6305010dd0a84272ccf9c38adaa346b9cc3089
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2024-07-14 15:11:10 +0300

    Fix test back

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ describe('App Component', () => {
     test('renders Vite + React heading', () => {
         render(<App />);
         const heading = screen.getByText('Vite + React');
-        expect(heading).not.toBeInTheDocument();
+        expect(heading).toBeInTheDocument();
     });
 });
